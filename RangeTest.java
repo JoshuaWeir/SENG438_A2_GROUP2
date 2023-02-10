19fe1d0af099b1e3a598833ff05406bf9c6b00ad
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2023-02-09 17:33:37 -0700

    Update RangeTest.java

--- RangeTest.java
+++ RangeTest.java
@@ -4,12 +4,17 @@ import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.
 
 public class RangeTest {
     private Range exampleRange;
+    private Range demoRange;
+    private Range nullRange;
+	
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
-
     @Before
-    public void setUp() throws Exception { exampleRange = new Range(-1, 1);
+    public void setUp() throws Exception { 
+	exampleRange = new Range(-1, 1);
+	demoRange = new Range (-5,5); 
+	nullRange = null;
     }
 
     @Test
@@ -131,6 +136,86 @@ public class RangeTest {
         assertEquals("The lower bound value of 1 and 1 should be 1",
         1, example.getLowerBound(), .000000001d);
     }
+	
+    //contains() tests
+	@Test
+	public void containsValueInRange() {
+		assertTrue("Value of 3 should be true as it is in range.", demoRange.contains(3));
+	}
+	@Test
+	public void containsValueOutsideRangeHigh() {
+		assertFalse("Value of 8 should be false as it is not in range.", demoRange.contains(8));
+	}
+	@Test
+	public void containsValueOutsideRangeLow() {
+		assertFalse("Value of -8 should be false as it is not in range.", demoRange.contains(-8));
+	}
+	@Test
+	public void containsValueIsLowerBound() {
+		assertTrue("Value of -5 should be true as it is in range.", demoRange.contains(-5));
+	}
+	@Test
+	public void containsValueIsUpperBound() {
+		assertTrue("Value of 5 should be true as it is in range.", demoRange.contains(5));
+	}
+	
+	//toString() tests
+	@Test
+	public void correctString() {
+		assertEquals("Function should return correct string.","Range[-5.0,5.0]",demoRange.toString());
+	}
+	@Test
+	public void nullString() {
+		assertNull(nullRange.toString());
+	}
+	
+	//intersects() tests
+	@Test
+	public void intersectLowerBound() {
+		assertTrue("Range should overlap on lower bound.",demoRange.intersects(-10, -5));
+	}
+	@Test
+	public void intersectUpperBound() {
+		assertTrue("Range should overlap on upper bound.",demoRange.intersects(5, 15));
+	}
+	@Test
+	public void intersectLowerHalf() {
+		assertTrue("Range should overlap on lower half.",demoRange.intersects(-10, -2));
+	}
+	@Test
+	public void intersectUpperHalf() {
+		assertTrue("Range should overlap on upper half.",demoRange.intersects(2, 10));
+	}
+	@Test
+	public void noIntersectLower() {
+		assertFalse("Range should not overlap under the lower bound.",demoRange.intersects(-15, -7));
+	}
+	@Test
+	public void noIntersectUpper() {
+		assertFalse("Range should not overlap over the upper bound.",demoRange.intersects(7, 15));
+	}
+	@Test
+	public void oneValueInRange() {
+		assertTrue("Range of (3,3) should intersect.",demoRange.intersects(3, 3));
+	}
+	@Test
+	public void oneValueNotInRange() {
+		assertFalse("Range of (8,8) should not intersect.",demoRange.intersects(8, 8));
+	}
+	@Test
+	public void noIntersectInNullRange() {
+		assertFalse("Null range should not have any intersect.",nullRange.intersects(0, 5));
+	}
+	@Test
+	public void completeIntersectInside() {
+		assertTrue("Range encapsulated inside should intersect.",demoRange.intersects(-3, 3));
+	}
+	@Test
+	public void completeIntersectOutside() {
+		assertTrue("Range encapsulating outside should intersect.",demoRange.intersects(-13, 13));
+	}
+	
+
 
     @After
     public void tearDown() throws Exception {
